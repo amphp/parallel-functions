74c8b5f145ce73c5c0cd55cc6d3e86f7d521ad52
Niklas Keller <me@kelunik.com>
2017-12-13 23:40:53 +0100

    Fix intentional example error and improve Travis build
    Examples are now executed in a subshell, so a failure doesn't terminate the complete build.

--- examples/2-echo.php
+++ examples/2-echo.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use function Amp\ParallelFunctions\parallelMap;
 use function Amp\Promise\wait;
-f
+
 // All output in the parallel environment is redirected to STDERR of the parent process automatically.
 // You might notice that the output order varies here when running it multiple times.
 wait(parallelMap([1, 2, 3], 'var_dump'));
