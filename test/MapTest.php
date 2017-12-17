999ba8a00adaf4d1fd3a7cb40bf7e565e507ff48
Niklas Keller <me@kelunik.com>
2017-12-17 19:33:29 +0100

    Fix code style

--- test/MapTest.php
+++ test/MapTest.php
@@ -16,7 +16,7 @@ class MapTest extends TestCase {
 
     public function testCorrectOutputOrder() {
         $this->assertSame([0, 1, 0], wait(parallelMap([0, 1, 0], function ($input) {
-            sleep($input);
+            \sleep($input);
 
             return $input;
         })));
