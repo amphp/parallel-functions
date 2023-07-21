436982f901ebd654f36677d4cfb7aa3e9494f4ca
Daniil Gentili <daniil@daniil.it>
2023-07-21 09:21:57 +0200

    Cleanup

--- examples/1-simple-function.php
+++ examples/1-simple-function.php
@@ -9,8 +9,7 @@ $start = microtime(true);
 
 // sleep() is executed in child processes, the results are sent back to the parent.
 //
-// All communication is non-blocking and can be used in an event loop. Amp\Promise\wait() can be used to use the library
-// in a traditional synchronous environment.
-wait(parallelMap([1, 2, 3], 'sleep'));
+// All communication is non-blocking and can be used in an event loop.
+parallelMap([1, 2, 3], 'sleep');
 
 print 'Took ' . (microtime(true) - $start) . ' seconds.' . \PHP_EOL;
--- examples/3-return-value.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-require __DIR__ . '/../vendor/autoload.php';
-
-use function Amp\ParallelFunctions\parallelMap;
-use function Amp\Promise\wait;
-
-// We have seen that the order can vary in the previous example, values returned have a deterministic order.
-var_dump(wait(parallelMap([1, 2, 3], 'abs')));
